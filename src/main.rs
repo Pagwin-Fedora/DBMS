c8485a527aeb2d09fcc760fa03ccdcf84f658102
Pagwin <git@pagwin.xyz>
2022-02-01 16:41:06 -0500

    almost functional

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
 extern crate clap;
-#[macro_use]
 extern crate serenity;
 extern crate dirs;
 extern crate futures;
+
+use std::process;
 
 use clap::{App, Arg, ArgMatches};
 use futures::executor::block_on;
@@ -11,54 +12,97 @@ use serenity::{Client, model::id::ChannelId, client::EventHandler};
 use serenity::prelude::*;
 fn main() {
     let send = App::new("send")
-        .about("Send a message");
+        .about("Send a message")
+        .arg(Arg::new("message text")
+            .required(true)
+            .long("text")
+            .short('t'));
     let edit = App::new("edit")
-        .about("Edit a message");
+        .about("Edit a message")
+        .arg(Arg::new("message id")
+            .required(true)
+            .long("message-id")
+            .short('m'))
+        .arg(Arg::new("message text")
+            .long("text")
+            .short('t'));
     let delete = App::new("delete")
-        .about("Delete a message");
-    let matches = App::new("Message edit shim")
+        .about("Delete a message")
+        .arg(Arg::new("message id")
+            .required(true)
+            .long("message-id")
+            .short('m'));
+    let app = App::new("Message edit shim")
         .version("0.1")
         .author("Pagwin <dev@pagwin.xyz>")
         .arg(Arg::new("config file")
-            .short('c')
             .long("config")
             )
         .arg(Arg::new("api token")
             .short('a')
             .long("api-token"))
+        .arg(Arg::new("channel id")
+            .short('c')
+            .long("channel-id")
+            .required(true))
         .subcommand(send)
         .subcommand(edit)
-        .subcommand(delete)
-        .get_matches();
-    let mut client = block_on(Client::builder(get_token(&matches))).unwrap();
-    //how I'm going to do this
+        .subcommand(delete);
+    let matches = app.get_matches();
+    //pretty sure I want to redo this so I use the derive method so I can just use an enum for the
+    //subcommands or maybe there's a way to do it here and I'm dumb
     let handler:Handler = match matches.subcommand(){
-        Some(("send",_)) =>{
-            Handler {
-                
-            }
+        Some(("send",submatch)) =>{
+            Some(Handler {
+                channel_id: matches.value_of("channel id").unwrap().parse().expect("invalid channel id"),
+                action: Action::Send(submatch.value_of("message text").unwrap().to_string())
+            })
         },
-        Some(("edit",_)) =>{},
-        Some(("delete",_)) => {},
-        Some(_)=>{}
-        None => {}
-    }
-    block_on(client.start());
+        Some(("edit",submatch)) =>{
+            Some(Handler {
+                channel_id: matches.value_of("channel id").unwrap().parse().expect("invalid channel id"),
+                action: Action::Edit(
+                    submatch.value_of("message id").unwrap().parse().expect("invalid message id"),
+                    submatch.value_of("message text").unwrap().to_string())
+            })
+        },
+        Some(("delete",submatch)) => {
+            Some(Handler {
+                channel_id: matches.value_of("channel id").unwrap().parse().expect("invalid channel id"),
+                action: Action::Delete(submatch.value_of("message id").unwrap().parse().expect("invalid message id"))
+            })
+        },
+        Some(_)=>{None}
+        None => {None}
+    }.unwrap();
+    let mut client = block_on(Client::builder(get_token(&matches))
+        .event_handler(handler)).unwrap();
+    block_on(client.start()).unwrap();
 }
 fn get_token(matches:&ArgMatches)->&str{
     matches.value_of("api token").unwrap_or_else(||{matches.value_of("config file").expect("api token or config file not provided")})
 }
-enum Action{Send, Edit, Delete}
+enum Action{Send(String), Edit(u64,String), Delete(u64)}
 struct Handler{
-    channelId:u64,
-    messageId:Option<u64>,
+    channel_id:u64,
     action:Action
 }
 #[serenity::async_trait]
 impl EventHandler for Handler {
-    async fn ready(&self, context: Context, ready:Ready){
-        let test_channel = ChannelId(self.channelId);
-        //test_channel.say(context,"test")
-        
+    async fn ready(&self, context: Context, _ready:Ready){
+        let channel = ChannelId(self.channel_id);
+        match &self.action {
+            Action::Send(message) => {
+                channel.say(context, message).await.unwrap();
+            },
+            //messy clones ick
+            Action::Edit(message_id, new_message) => {
+                channel.edit_message(context, message_id.clone(), |m| m.content(new_message)).await.unwrap();
+            },
+            Action::Delete(message_id) => {
+                channel.delete_message(context, message_id.clone()).await.unwrap();
+            }
+        }
+        process::exit(0);
     }
 }
